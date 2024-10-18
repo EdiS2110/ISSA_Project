ff53dca5fbc6488a1a08c381f8c271945c5bd7ae
EdiS2110 <edi.samachis@gmail.com>
2024-10-18 14:28:32 +0300

    First touch

---
+++ LaneDetection/main.py
@@ -0,0 +1,30 @@
+from random import random
+
+import cv2
+import numpy as np
+from cv2 import cvtColor
+
+cam = cv2.VideoCapture('Lane Detection Test Video 01 1.mp4')
+
+width = 1920/3
+height = 1080/4
+
+while True:
+    ret, frame = cam.read()
+
+    if ret is False:
+        break
+
+    frame =  cv2.resize(frame, (640, 270))
+
+
+    frame = cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Original', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
